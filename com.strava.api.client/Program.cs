6638200cf1a191ad69c830daf8ff87aca015b286
inexcitus <misc@sascha-simon.com>
2014-01-30 20:39:53 +0100

    GetRequest ist nun async

--- com.strava.api.client/Program.cs
+++ com.strava.api.client/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using com.strava.api.Http;
 
 namespace com.strava.api.client
@@ -11,11 +7,14 @@ namespace com.strava.api.client
     {
         public static void Main(String[] args)
         {
-            GetRequest r = new GetRequest();
-            r.Execute("");
-
-
+            Test();
             Console.ReadLine();
+        }
+
+        public static async void Test()
+        {
+            var response = await GetRequest.ExecuteAsync("https://www.strava.com/api/v3/activities/109557593?access_token=72e8fa9d4f63477adc76555de382a033b6aedf6d");
+            Console.WriteLine(response);
         }
     }
 }
--- com.strava.api/Http/GetRequest.cs
+++ com.strava.api/Http/GetRequest.cs
@@ -1,45 +1,32 @@
 ﻿using System;
 using System.IO;
 using System.Net;
-using System.Text;
-using com.strava.api.Activities;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace com.strava.api.Http
 {
-    public class GetRequest
+    public static class GetRequest
     {
-        public GetRequest()
+        public static async Task<String> ExecuteAsync(string uri)
         {
-            
-        }
-
-        public void Execute(string requestUri)
-        {
-            if (String.IsNullOrEmpty(requestUri))
+            if (String.IsNullOrEmpty(uri))
                 throw new ArgumentException("Parameter requestUri can not be null or empty. Please commit a valid Uri.");
+            
+            //  Anfrage
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
+            WebResponse response = await request.GetResponseAsync();
 
-            // GET https://www.strava.com/api/v3/activities/109557593?access_token=72e8fa9d4f63477adc76555de382a033b6aedf6d
-            // 72e8fa9d4f63477adc76555de382a033b6aedf6d
-
-            HttpWebRequest request = (HttpWebRequest)System.Net.WebRequest.Create(new Uri("https://www.strava.com/api/v3/activities/109557593?access_token=72e8fa9d4f63477adc76555de382a033b6aedf6d"));
-            Stream stream = request.GetResponse().GetResponseStream();
-
-            StreamReader reader = new StreamReader(stream);
-
-            StringBuilder json = new StringBuilder();
-
-            while (!reader.EndOfStream)
+            //  Auslesen
+            if (response != null)
             {
-                json.Append(reader.ReadLine());
+                Stream stream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(stream);
+
+                return reader.ReadToEnd();
             }
 
-            var root = JsonConvert.DeserializeObject<Activity>(json.ToString());
 
-            Console.WriteLine(root.Name);
-            Console.WriteLine("HR: " + root.AverageHeartrate);
-            Console.WriteLine("Max HR: " + root.MaxHeartrate);
+            return String.Empty;
         }
     }
 }
